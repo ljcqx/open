92ee9396b60e73ced1e31f6d9e971784a1f1e5dd
sunkang <68103403@qq.com>
2012-05-31 16:16:31 +0800

    my.show.js

--- js/my.tab.js
+++ js/my.tab.js
@@ -5,6 +5,7 @@ $(function(){
 	 //or 
 	 
 	  $('ul').tab({event:'hover'}); 
+	  $('.tab').tab({hide:true});
 	 
 }); 
 *
@@ -20,21 +21,23 @@ $(function(){
 		tab: function(options) { 
 			var defaults = {
 				active:'active',
-				event: 'click'
+				event: 'click',
+				hide: false
 			};
 			var opts = $.extend(defaults,options);  
 			var obj = this;
 			var at = opts.active; //actived class 
 			var e = opts.event; // event,like click,hover
+			var hidden = opts.hide;
 			var i = 0;
 			var j = 0;
-			$(this).find('li a').each(function(){ 
-				selected(i,this);
+			$(this).find('a').each(function(){ 
+				selected(i,this,hidden); 
 				i++;
 				$(this).attr('href','javascript:void(0);').attr('onclick',"return false");
 			}).bind(e,function(){
 				 var h = "#"+$(this).attr('rel'); 
-				 $(obj).find('li a').each(function(){ 
+				 $(obj).find('a').each(function(){ 
 					var current = "#"+$(this).attr('rel');  
 					if(h ==  current ){  
 						$(h).show();
@@ -46,9 +49,9 @@ $(function(){
 				})
 
 			});
-			function selected(i,obj){ 
-				var h = "#"+$(obj).attr('rel'); 
-				if(i==0){
+			function selected(i,obj,hidden){ 
+				var h = "#"+$(obj).attr('rel');  
+				if(hidden==false && i==0){
 					$(obj).addClass(at);
 					$(h).show();
 				}else{
