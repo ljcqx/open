be7cc7cce72c71535623f67d063f38acdfe6c081
sunkang <68103403@qq.com>
2012-06-12 21:55:58 +0800

    map

---
+++ js/map.js
@@ -0,0 +1,19 @@
+$(function(){
+ 	$('.map li').click(function(){
+ 		$('.map_hide').hide();
+ 		$('#'+$(this).attr('rel')).show();
+ 		
+ 	});
+ 	
+ 	$('.map_show').hover(function(){
+ 		var j = $(this).attr('coords').split(',');
+	 	var top=parseFloat(j[1])+parseFloat(26);
+ 		$('#port_'+$(this).attr('rel')).show()
+ 				.css('position','absolute')
+ 				.css('top',top+'px')
+ 				.css('left',j[0]+'px');
+ 	}).mouseout(function(){
+ 	 	$('#port_'+$(this).attr('rel')).hide();
+ 	});
+
+});
