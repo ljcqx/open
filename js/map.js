81444e0f49df18d7a0e0ca409dba44de9fbac31f
sunkang <68103403@qq.com>
2012-06-14 16:31:57 +0800

    map.js mouseover

--- js/map.js
+++ js/map.js
@@ -5,16 +5,25 @@ $(function(){
  		$('.map li').removeClass('active');
  		$(this).addClass('active');
  	});
- 	
+ 	var s;
  	$('.map_show').hover(function(){
  		var j = $(this).attr('coords').split(',');
 	 	var top=parseFloat(j[1])+parseFloat(26);
+	 	s = '#port_'+$(this).attr('rel');
  		$('#port_'+$(this).attr('rel')).show()
  				.css('position','absolute')
  				.css('top',top+'px')
  				.css('left',j[0]+'px');
  	}).mouseout(function(){
- 	 	$('#port_'+$(this).attr('rel')).hide();
+ 	 	$(s).mouseover(function(){
+ 	 		$(s).show();
+ 	 	}).mouseout(function(){
+ 	 		$(s).hide();
+ 	 	});
+ 	 	
+ 	 	$(s).hide();
  	});
+ 
+
 
 });
