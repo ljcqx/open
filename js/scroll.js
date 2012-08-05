0bdd506b9283e7515e7e02183fd90a1abadf5d93
unknown <admin@admin-PC.(none)>
2012-08-05 21:10:05 +0800

    add scroll.js

---
+++ js/scroll.js
@@ -0,0 +1,39 @@
+/*
+<script src="js/jquery.min.js"></script>
+<script src="js/scroll.js"></script>
+<script>
+$(function(){ 
+	 scroll(".mallMiddleBottom",".mallLeftImg",".mallRightImg",".mallUl",3,true);
+
+});
+</script>
+ */
+ function scroll(box,left,right,img,speed,or)
+ { 
+  var box = $(box);
+  var left = $(left);
+  var right = $(right);
+  var img = $(img).find('ul');
+  var w = img.find('li').outerWidth(true);
+  var s = speed;
+  left.click(function()
+	   {
+		img.animate({'margin-left':-w},function()
+				  {
+				   img.find('li').eq(0).appendTo(img);
+				   img.css({'margin-left':0});
+				   });
+		});
+  right.click(function()
+	   {
+		img.find('li:last').prependTo(img);
+		img.css({'margin-left':-w});
+		img.animate({'margin-left':0});
+		});
+  if (or == true)
+  {
+   ad = setInterval(function() { right.click();},s*1000);
+   box.hover(function(){clearInterval(ad);},function(){ad = setInterval(function() { right.click();},s*1000);});
+  }
+ }
+ 
